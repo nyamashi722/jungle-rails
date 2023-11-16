c719070e154e207f4f2e46d6f5275b5b1319fc3d
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-16 10:41:30 -0500

    create query to return number of products and categories from db

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'], only: :show
   def show
+    @num_of_products = Product.count
+    @num_of_categories = Category.count
   end
 end
